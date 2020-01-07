5dae8b4d6f45bb79b4f9f48a2072aa32faea62c1
Abdul Khan <abdulk@gmail.com>
2020-01-07 13:23:44 -0600

    fixed bash script for extenstions

--- VSCode/Install_Plugin.bash
+++ VSCode/Install_Plugin.bash
@@ -1,47 +1,13 @@
-# Script for batch installing Visual Studio Code extensions
-# Specify extensions to be checked & installed by modifying $extensions
+#!/bin/bash
 
-extensions =(
-    # Look & Feel
-    "azemoh.one-monokai"
-    "emmanuelbeziat.vscode-great-icons"
-    # Editing
-    "HookyQR.beautify"
-    "ow.vscode-subword-navigation"
-    "formulahendry.auto-close-tag"
-    "formulahendry.auto-rename-tag"
-    # Language support
-    "ms-vscode.csharp"
-    # Extra functionality
-    "humao.rest-client"
-    "cssho.vscode-svgviewer"
-    # IaC pluggins
-    "jupyter.showPreview"
-    "eamodio.gitlens"
-    "mindginative.terraform-snippets"
-    "neilbarkhina.gitdownloadazurerepos"
-    "ms-azuretools.vscode-azureterraform"
-    "hookyqr.beautify"
-    "bibhasdn.git-easy"
-    "donjayamanne.githistory"
-    "knisterpeter.vscode-github"
-    "donjayamanne.jupyter"
-    "ms-vscode.powershell"
-    "loganarnett.tf-snippets"
-    "mccarter.start-git-bash"
-    "4ops.terraform"
-    "mauve.terraform"
-    "erd0s.terraform-autocomplete"
-    "laszer25.terraform-azure-autocomplete"
-    "sahmed.terraform-comments-beautifier")
-    
+#Declare a string array
+Extenstions=("azemoh.one-monokai" "emmanuelbeziat.vscode-great-icons" "HookyQR.beautify" "ow.vscode-subword-navigation" "formulahendry.auto-close-tag" "formulahendry.auto-rename-tag" "ms-vscode.csharp" "humao.rest-client" "cssho.vscode-svgviewer" "jupyter.showPreview" "eamodio.gitlens" "mindginative.terraform-snippets" "neilbarkhina.gitdownloadazurerepos" "ms-azuretools.vscode-azureterraform" "hookyqr.beautify" "bibhasdn.git-easy" "donjayamanne.githistory" "knisterpeter.vscode-github" "donjayamanne.jupyter" "ms-vscode.powershell" "loganarnett.tf-snippets" "mccarter.start-git-bash" "4ops.terraform" "mauve.terraform" "erd0s.terraform-autocomplete" "laszer25.terraform-azure-autocomplete" "sahmed.terraform-comments-beautifier")
 
-
-
+# Print array values in  lines
 echo "Installing Extenstions"
-for val1 in ${extensions[*]}; do
+for val1 in ${Extenstions[*]}; do
      echo $val1
      code --install-extension $val1
 done
- 
+
 echo ""
